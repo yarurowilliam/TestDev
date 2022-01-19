f78f69952a919962f8b65d5e6d655418d221161a
PowtherW <wayaruro@unicesar.edu.co>
2022-01-19 02:37:57 -0500

    DevTest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Movie } from './models/movies';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TestDev';
+export class AppComponent implements OnInit {
+  listMovies: Movie[] = [];
+  datosMovie: FormGroup;
+
+  constructor(private fb: FormBuilder, private toastr: ToastrService) {
+    this.datosMovie = this.fb.group({
+      titulo: ['', Validators.required],
+      year: ['', Validators.required],
+      genre: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+  
+  registrarMovie(): void{
+    const movie: Movie ={
+      title: this.datosMovie.value.titulo,
+      year: this.datosMovie.value.year,
+      genre: this.datosMovie.value.genre
+    };
+    
+    if (movie.title.trim() == "" || movie.genre.trim() == ""){
+      this.toastr.error('No se permiten espacios en blancos', 'Error!');
+    }else{
+      this.listMovies.push(movie);
+      this.toastr.success('La movie ' + movie.title + ' fue registrada con exito!', 'Movie Registrada!');
+      this.datosMovie.reset();
+    }   
+  }
+
+  eliminarMovie(indice: number): void{
+    this.listMovies.splice(indice,1);
+    this.toastr.info('La movie fue eliminada' , 'Movie Eliminada!');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ToastrModule } from 'ngx-toastr';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
